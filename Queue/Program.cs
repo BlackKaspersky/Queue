cf80c3e0b6d1b25c28d71241e09d3455ffa2bef1
Мартин Симонян <simonyan.martin@rambler.ru>
2023-04-28 17:49:44 +0300

    Implemented queue

--- Queue/Program.cs
+++ Queue/Program.cs
@@ -1,10 +1,23 @@
 ﻿namespace Queue
 {
-    internal class Program
+    public  class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+
+            Console.WriteLine(queue.Contain(3));
+            Console.WriteLine(queue.Dequeue());
+            foreach(var i in queue)
+            {
+                Console.Write(i + " ");
+            }
+
         }
     }
 }
---
+++ Queue/Queue.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+
+namespace Queue
+{
+    public class Queue<T> : IEnumerable<T>
+    { 
+        int count;
+        Node<T>? head;
+        Node<T>? tail;
+
+
+        public int Count { get { return count; } }
+        
+        public bool IsEmpty { get { return count == 0; } }
+
+
+        public void Enqueue(T data)
+        {
+            var node = new Node<T>(data);
+            if (head == null)
+                head = node;
+            else
+                tail!.Next = node;
+            tail = node;
+            count++;
+        }
+
+        public T Dequeue()
+        {
+            if (IsEmpty)
+                throw new InvalidDataException();
+            var headData = head!.Data;
+            head = head.Next;
+            count--;
+            return headData;
+        }
+
+        public T First
+        {
+            get
+            {
+                if (IsEmpty)
+                    throw new InvalidDataException();
+                return head!.Data;
+            }
+        }
+
+        public T Last
+        {
+            get
+            {
+                if (IsEmpty)
+                    throw new InvalidDataException();
+                return tail!.Data;
+            }
+        }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public bool Contain(T data)
+        {
+            var current = head;
+            while(current != null)
+            {
+                if(current.Data.Equals(data))
+                    return true;
+
+                current = current.Next;
+            }
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)this).GetEnumerator();
+        }
+
+        IEnumerator<T> IEnumerable<T>.GetEnumerator()
+        {
+            Node<T> current = head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+    }
+}
